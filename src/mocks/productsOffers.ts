52164e0bca570f3ef9111058c916451a3f7bad28
Daniel Filgueira <dfsdireito95@gmail.com>
2022-12-27 00:31:35 -0300

    :pencil: adding product offers data mock

--- src/mocks/productsOffers.ts
+++ src/mocks/productsOffers.ts
@@ -1,28 +1,61 @@
-import { ProductProps } from "src/components/ProductCard/types";
+import { ProductProps } from 'src/components/ProductCard/types';
 
 export const productsOffers: ProductProps[] = [
   {
-    price: "R$ 250,00",
-    promotionalPrice: "R$ 200,00",
-    title: "Garrafa Térmica VIP",
+    price: 'R$ 250,00',
+    discountText: '50%',
+    promotionalPrice: 'R$ 200,00',
+    title: 'Garrafa Térmica VIP',
     soldItems: 80,
     totalItems: 100,
-    type: 'Personal'
+    type: 'Personal',
+    imgSrc:
+      'https://images.unsplash.com/photo-1589365278144-c9e705f843ba?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
   },
   {
-    price: "R$ 150,00",
-    promotionalPrice: "R$ 100,00",
-    title: "Camiseta Lisa",
+    price: 'R$ 150,00',
+    discountText: '50%',
+    promotionalPrice: 'R$ 100,00',
+    title: 'Camiseta Lisa',
     soldItems: 50,
     totalItems: 70,
-    type: 'Personal'
+    type: 'Personal',
+    favorite: true,
+    imgSrc:
+      'https://images.unsplash.com/photo-1602143407151-7111542de6e8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
   },
   {
-    price: "R$ 50,00",
-    promotionalPrice: "R$ 30,00",
-    title: "Plantinha Aloe Vera",
+    price: 'R$ 50,00',
+    discountText: '50%',
+    promotionalPrice: 'R$ 30,00',
+    title: 'Plantinha Aloe Vera',
     soldItems: 8,
     totalItems: 10,
-    type: 'Personal'
+    type: 'Personal',
+    imgSrc:
+      'https://images.unsplash.com/photo-1544003484-3cd181d17917?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80',
+  },
+  {
+    price: 'R$ 50,00',
+    discountText: '50%',
+    promotionalPrice: 'R$ 30,00',
+    title: 'Plantinha Aloe Vera',
+    soldItems: 8,
+    totalItems: 10,
+    type: 'Personal',
+    imgSrc:
+      'https://images.unsplash.com/photo-1618354691249-18772bbac3a5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
+  },
+
+  {
+    price: 'R$ 50,00',
+    discountText: '50%',
+    promotionalPrice: 'R$ 30,00',
+    title: 'Plantinha Aloe Vera',
+    soldItems: 8,
+    totalItems: 10,
+    type: 'Personal',
+    imgSrc:
+      'https://images.unsplash.com/photo-1590506231970-b268a0fde7b6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
   },
 ];
